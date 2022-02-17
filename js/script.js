d1010103501576837dbec923683da312e9301612
SumiAkter2 <akteralisha36@gmail.com>
2022-02-17 16:17:08 +0600

    handle error

--- js/script.js
+++ js/script.js
@@ -1,68 +1,8 @@
 console.log('success');
 
-// document.getElementById('calc-btn').addEventListener('click', function(){
-//     const foodInput = document.getElementById('food-id');
-//     const foodCost =parseFloat (foodInput.value);
-//     console.log(foodCost)
 
-//     const rentInput = document.getElementById('rent-id');
-//     const rentCost =parseFloat (rentInput.value);
-//     console.log(rentCost)
-
-//     const clothInput = document.getElementById('cloth-id');
-//     const clothCost =parseFloat (clothInput.value);
-//     console.log(clothCost)
-
-   
-//     let total = foodCost + rentCost + clothCost;
-//     console.log(total);
-
-//  const totalExpense= document.getElementById('total-expenses');
-//  const totalExpenseValue = totalExpense.innerText;
-//  totalExpense.innerText=total;
-//  console.log(total);
-//  const incomeInput =document.getElementById('income-id');
-//  const incomeValue= parseFloat (incomeInput.value);
-//  console.log(incomeValue)
-//    const balance = document.getElementById('balance');
-//    const balanceValue = balance.innerText;
-//   const totalBalance = incomeValue - total;
-//   balance.innerText = totalBalance;
-//   console.log(totalBalance);        
-
-// getSavings ('total-expenses', 'balance');
-// });
-// function getInputValue(totalId,amount){
-//     const totalExpense= document.getElementById(totalId);
-//     const totalExpenseValue = totalExpense.innerText;
-//     totalExpense.innerText=total;
-
-//     const incomeInput =document.getElementById('income-id');
-//     const incomeValue= parseFloat (incomeInput.value);
-//     console.log(incomeValue);
-    
-//     const balance = document.getElementById(amount);
-//     const balanceValue = balance.innerText;
-//    const totalBalance = incomeValue - total;
-//    balance.innerText = totalBalance;
-//    console.log(totalBalance);
-// }
-   
-
-
-// function getSavings(saving){
-//     const saveAmountInput= document.getElementById(saving);
-//     const saveAmountValue = parseFloat(saveAmountInput.value);
-//     console.log(saveAmountValue);
-
-   
-// }
-// document.getElementById('save-btn').addEventListener('click', function(){
-    
-//    getSavings('save-id')
-// })
-
-function calcBtn( saving, saveAmount , remainBalance){
+function calculation( saving, saveAmount, remainBalance){
+    // food, rent , cloth cost:
     let foodInput = document.getElementById('food-id').value;
     console.log(foodInput);
     let rentInput = document.getElementById('rent-id').value;
@@ -76,12 +16,15 @@ function calcBtn( saving, saveAmount , remainBalance){
 //  else{
 //      console.log('sorry')
 //  }
+//  ----------total expenses:------
 const totalExpense = document.getElementById('total-expenses');
 const totalExpenseValue = totalExpense.innerText;
 totalExpense.innerText = totalInput;
 console.log(totalInput);
-
+// -----------income input show----------
 const incomeInput = document.getElementById('income-id').value;
+
+// ------------total balance show -----
 
 const balance = document.getElementById('balance');
 const balanceValue=balance.innerText;
@@ -89,22 +32,60 @@ const totalBalance = parseInt(incomeInput)-totalInput;
  balance.innerText = totalBalance;
  console.log(totalBalance);
 
+ //---------saving section------
+
  const savingInput = document.getElementById(saving);
  const savingValue = savingInput.value;
-console.log(savingValue);
+
 const saveAmountValue = document.getElementById(saveAmount);
 const saveAmountInput = saveAmountValue.innerText;
-const savingCost = totalBalance/savingValue;
-saveAmountValue.innerText = savingCost;
-console.log(savingCost);
 
+ const savingCost = totalBalance/savingValue;
+ saveAmountValue.innerText = savingCost;
+if( totalBalance< savingValue){
+    
+    document.getElementById('hide-cuppon').style.display='block';
+}
+if(savingValue!=Number){
+    document.getElementById('hide-cuppon').style.display='block';
+ }
+if( totalBalance> savingValue){
+    saveAmountValue.innerText = savingCost;
+    document.getElementById('hide-cuppon').style.display='none';
+}
+
+
+
+//--------after saving remaining balance section---------
 const remainingInput = document.getElementById(remainBalance);
 const remainingInputValue = remainingInput.innerText;
-const remainingBalance = totalBalance- savingCost;
-remainingInput.innerText = remainingBalance;
-console.log(remainingBalance);
+ const remainingBalance = totalBalance- savingCost;
+ remainingInput.innerText = remainingBalance;
 
+ if( totalBalance < savingValue){
+    
+    document.getElementById('hide-cuppon').style.display='block';
+ }
+ if(savingValue!=Number){
+    document.getElementById('hide-cuppon').style.display='block';
+ }
+ if (totalBalance > savingValue){
+    remainingInput.innerText = remainingBalance;
+    document.getElementById('hide-cuppon').style.display='none';
+ }
+else{
+   
+    remainingInput.innerText = remainingBalance;
+    document.getElementById('hide-cuppon').style.display='none';
 }
+
+console.log(remainingBalance);
+}
+// calculation
+document.getElementById('calc-btn').addEventListener('click', function(){
+    calculation();
+});
+//saving
 document.getElementById('save-btn').addEventListener('click', function(){
-calcBtn('save-id','saving-amount', 'remaining-balance')
-})
+calculation('save-id','saving-amount', 'remaining-balance');
+});
