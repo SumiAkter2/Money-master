7050759c00e20a9ea806fd4bbe233dd025dd7ca3
SumiAkter2 <akteralisha36@gmail.com>
2022-02-17 17:15:23 +0600

    saving error handle

--- js/script.js
+++ js/script.js
@@ -12,24 +12,49 @@ function calculation( saving, saveAmount, remainBalance){
     //  if(foodInput>0 && rentInput >0 && clothInput>0){
     let totalInput = parseInt(foodInput)+parseInt(rentInput)+parseInt(clothInput);
 //     console.log(totalInput);
-//  }
-//  else{
-//      console.log('sorry')
-//  }
+
 //  ----------total expenses:------
 const totalExpense = document.getElementById('total-expenses');
 const totalExpenseValue = totalExpense.innerText;
-totalExpense.innerText = totalInput;
+totalExpense.innerText = "00";
+
 console.log(totalInput);
+
+if(foodInput<0 || rentInput<0 || clothInput<0){
+    document.getElementById('expenses-hide').style.display='block';
+} 
+//  else{
+//      document.getElementById('expenses-hide').style.display='none';
+//      totalExpense.innerText = totalInput;
+//  }
 // -----------income input show----------
 const incomeInput = document.getElementById('income-id').value;
 
+   if(incomeInput<foodInput || incomeInput<rentInput || incomeInput<clothInput) {
+     document.getElementById('expenses-hide').style.display='block';
+ }
+  else{
+     document.getElementById('expenses-hide').style.display='none';
+      totalExpense.innerText = totalInput;
+  }
 // ------------total balance show -----
 
 const balance = document.getElementById('balance');
 const balanceValue=balance.innerText;
 const totalBalance = parseInt(incomeInput)-totalInput;
- balance.innerText = totalBalance;
+balance.innerText = "00";
+ if(incomeInput<foodInput || incomeInput<rentInput || incomeInput<clothInput){
+    document.getElementById('expenses-hide').style.display='block';
+ }
+else if(foodInput<0 || rentInput<0 || clothInput<0){
+    document.getElementById('expenses-hide').style.display='block';
+} 
+else{
+    document.getElementById('expenses-hide').style.display='none';
+    balance.innerText = totalBalance;
+}
+
+//  balance.innerText = totalBalance;
  console.log(totalBalance);
 
  //---------saving section------
@@ -39,20 +64,21 @@ const totalBalance = parseInt(incomeInput)-totalInput;
 
 const saveAmountValue = document.getElementById(saveAmount);
 const saveAmountInput = saveAmountValue.innerText;
-
+saveAmountValue.innerText =" 00";
  const savingCost = totalBalance/savingValue;
- saveAmountValue.innerText = savingCost;
+//  saveAmountValue.innerText = savingCost;
+//------------------error handle--------------
 if( totalBalance< savingValue){
     
-    document.getElementById('hide-cuppon').style.display='block';
-}
-if(savingValue!=Number){
-    document.getElementById('hide-cuppon').style.display='block';
+     document.getElementById('hide-cuppon').style.display='block';
  }
-if( totalBalance> savingValue){
-    saveAmountValue.innerText = savingCost;
-    document.getElementById('hide-cuppon').style.display='none';
-}
+  else if(savingValue<0){
+      document.getElementById('hide-cuppon').style.display='block';
+    }
+else {
+     saveAmountValue.innerText = savingCost;
+     document.getElementById('hide-cuppon').style.display='none';
+ }
 
 
 
@@ -60,24 +86,23 @@ if( totalBalance> savingValue){
 const remainingInput = document.getElementById(remainBalance);
 const remainingInputValue = remainingInput.innerText;
  const remainingBalance = totalBalance- savingCost;
- remainingInput.innerText = remainingBalance;
-
- if( totalBalance < savingValue){
+//  remainingInput.innerText = remainingBalance;
+remainingInput.innerText =" 00";
+//----------------error handle------------------
+  if( totalBalance < savingValue){
     
-    document.getElementById('hide-cuppon').style.display='block';
- }
- if(savingValue!=Number){
-    document.getElementById('hide-cuppon').style.display='block';
- }
- if (totalBalance > savingValue){
-    remainingInput.innerText = remainingBalance;
-    document.getElementById('hide-cuppon').style.display='none';
- }
-else{
+     document.getElementById('hide-cuppon').style.display='block';
+  }
+
+   else if(savingValue<0){
+       document.getElementById('hide-cuppon').style.display='block';
+    }
+
+ else{
    
-    remainingInput.innerText = remainingBalance;
-    document.getElementById('hide-cuppon').style.display='none';
-}
+     remainingInput.innerText = remainingBalance;
+     document.getElementById('hide-cuppon').style.display='none';
+     }
 
 console.log(remainingBalance);
 }
